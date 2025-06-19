3bf30ffc3c7815657cf4d0c080195e9630a33249
alessia-ippolito <alessia.ippo01@gmail.com>
2025-06-19 19:37:49 +0200

    Add files via upload

---
+++ map_page.py
@@ -0,0 +1,152 @@
+import dash
+import requests
+import pandas as pd
+from functions.logger import logger
+from dash import dcc
+from dash import html, Output, Input
+from components.map_component import create_map, create_layer_group
+from components.dropdown_component import create_dropdown
+from functions.fetch_pollutant import fetch_pollutant
+import plotly.express as px
+from datetime import datetime, timedelta
+
+dash.register_page(__name__, path="/map", name="Map")
+
+df_all = fetch_pollutant()
+pollutants =  sorted(df_all["nometiposensore"].dropna().unique()) if not df_all.empty else []
+
+# Visualization layout
+layout = html.Div([
+    html.H2("Air Quality Map", style={"textAlign": "center"}),
+    html.Div([
+        create_map(),
+        create_dropdown(pollutants)
+    ], style={
+        "position": "relative",
+        "borderRadius": "15px",
+        "overflow": "hidden",
+        "boxShadow": "0 2px 8px rgba(0,0,0,0.2)"
+    }),html.Div([
+
+    dcc.DatePickerRange( # DatePickerRange component, calendar for selecting a date range
+        id="date-picker-range",
+        min_date_allowed=datetime(2024, 12, 25),
+        max_date_allowed=datetime(2025, 1, 1),
+        start_date_placeholder_text="Start Period",
+        end_date_placeholder_text="End Period",
+        calendar_orientation='vertical',
+        style={
+            "border": "1px solid #ccc",
+            "borderRadius": "8px",
+            "padding": "10px",
+            "marginBottom": "20px",
+            "backgroundColor": "#ffffff",
+            "boxShadow": "0 2px 6px rgba(0, 0, 0, 0.1)",
+            "width": "100%",
+            "display": "flex",
+            "justifyContent": "center",
+            "marginTop": "20px"
+        }
+    ),
+    dcc.Graph( # Histogram component, bar chart for average pollutant values per province
+        id="histogram",
+        style={"height": "400px"}
+    )  
+    ])  
+    ], style={
+    "paddingLeft": "20px",
+    "paddingRight": "20px",
+    "maxWidth": "1200px",
+    "margin": "auto"})
+
+# Callback to update the map based on selected pollutant
+@dash.callback(
+    [Output("map", "children"),
+     Output("station-count", "children")],
+    [Input("pollutant-selector", "value")]
+)
+def update_map(selected_pollutant):
+    base_tile = create_map().children[0]
+
+    if selected_pollutant in [None, "Tutti"]:
+        df = fetch_pollutant()  # Nessun filtro
+        num_sensors = df.shape[0]
+        num_stations = df["nomestazione"].nunique()
+        layer = create_layer_group(df, selected_pollutant)
+        count_text = f"All pollutants – {num_sensors} sensors, {num_stations} stations"
+        return [base_tile, layer], count_text
+
+    else:
+        df = fetch_pollutant(selected_pollutant)
+        pollutant_label = f"Pollutant: '{selected_pollutant}'"
+        layer = create_layer_group(df, selected_pollutant)
+        if not df.empty:
+            num_stations = df["nomestazione"].nunique()
+            count_text = f"{pollutant_label} – {num_stations} stations"
+        else:
+            count_text = "No data."
+        return [base_tile, layer], count_text
+
+
+# ------ HISTOGRAM FUNCTIONS ------
+
+def fetch_avg_province_pollutant(pollutants, start_date=None, end_date=None):
+    """
+    Function to fetch pollutant data from the API, filters the data accordingly the time range.
+    """
+    try:
+        if pollutants == "Tutti" or pollutants is None:
+            pollutants = "Oddidi di Azoto"
+        if not start_date or not end_date:
+            res = requests.get("http://localhost:5000/api/avg_province_time",
+                               params={'pollutant': pollutants})
+        else:
+            res = requests.get("http://localhost:5000/api/avg_province_time", 
+                           params = {'pollutant': pollutants,'start_date': start_date, 'end_date': end_date})
+        df = pd.DataFrame(res.json())
+        return df
+    except Exception as e:
+        logger.error(f"Error fetching data: {e}")
+        return pd.DataFrame()
+
+# Callback to update the histogram based on selected pollutant and date range
+@dash.callback(
+    Output("histogram",  "figure"),
+    [Input("pollutant-selector", "value"),
+     Input("date-picker-range", "start_date"),
+     Input("date-picker-range", "end_date")]
+)
+def update_histogram(selected_pollutant, start_date, end_date):
+    """
+    Creates a histogram (bar chart) of average pollutant values per province.
+    """
+    if selected_pollutant == "Tutti" or selected_pollutant is None:
+        selected_pollutant = "Ossidi di Azoto"
+
+    df = fetch_avg_province_pollutant(selected_pollutant, start_date, end_date)
+    if df.empty:
+        return {
+            "data": [],
+            "layout": {
+                "title": "No data available for the selected pollutant and date range."
+            }
+        }
+    
+    unit = df["unitamisura"].iloc[0] if "unitamisura" in df.columns else "" # retrieve unit of measurement
+    fig = px.bar( # bar chart creation
+        df,
+        x="provincia",
+        y="mean", 
+        title=f"Average '{selected_pollutant}' per Province in {unit}",
+        labels={"provincia": "Province", "mean": "Average Value"},
+        template="plotly_white",  
+    )
+    fig.update_layout( # axis and title configuration 
+        xaxis_title="Province",
+        yaxis_title=f"Average {selected_pollutant} ({unit})",
+        title_x=0.5)
+    
+    return fig
+
+
+    
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Database connection function
 def db_connection():
     conn = psycopg2.connect(
         host="localhost",
@@ -13,6 +14,7 @@ def db_connection():
     )
     return conn
 
+# Endpoint to get pollutants data
 @app.route('/api/stations', methods=['GET'])
 def get_stations():
     pollutant = request.args.get('pollutant', default=None, type=str)  # load parameter
@@ -41,7 +43,9 @@ def get_stations():
         return jsonify(data)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
+# ---- Login and Signin ----
+# Function to check if a user exists    
 def check_user_exists(username):
     conn = db_connection()
     cursor = conn.cursor()
@@ -51,7 +55,7 @@ def check_user_exists(username):
     cursor.close()
     return 1 if result else 0
 
-#LOGIN
+# Login
 @app.route('/api/login', methods=['POST'])
 def login():
     conn = db_connection()
@@ -89,17 +93,15 @@ def login():
     else: 
         return jsonify({"message": "ERROR"}),500
 
-#SIGNIN
+# Signin
 @app.route('/api/signin', methods=['POST'])
 def signin():
-
     global logged_in
     conn = db_connection()
     new_data = request.get_json()
     username = new_data.get('username')
     email = new_data.get('email')
     password = new_data.get('password')
-
     # if the username or password or email is empty
     if not username or not password or not email:
         return jsonify({"message": "All fields are necessary to be registered"}), 400
@@ -116,7 +118,39 @@ def signin():
             return jsonify({"message": "Signin successful"}), 200
         except Exception as e:
             return jsonify({"message": f"Internal server error: {str(e)}"}), 500
-      
 
+# Endpoint to get average pollutant values by province and time     
+@app.route('/api/avg_province_time', methods=['GET'])
+def get_data_by_time():
+
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    pollutant = request.args.get('pollutant', default='Ossidi di Azoto')
+    try:
+        conn = db_connection()
+        if not start_date or not end_date:
+            query = """
+                SELECT s.unitamisura, s.provincia, AVG(m.valore) as mean
+                FROM measurement as m JOIN station as s ON m.idsensore = s.idsensore
+                WHERE s.nometiposensore = %s AND m.data >= (SELECT MAX(data) FROM measurement) - INTERVAL '7 days' 
+                AND m.stato = 'VA'
+                GROUP BY s.provincia, s.unitamisura;
+                """
+            df = pd.read_sql_query(query, conn, params=(pollutant,))    
+        else:
+            query = """
+                SELECT s.unitamisura, s.provincia, AVG(m.valore) as mean
+                FROM measurement as m JOIN station as s ON m.idsensore = s.idsensore
+                WHERE s.nometiposensore = %s AND m.data >= %s AND m.data <= %s
+                AND m.stato = 'VA'
+                GROUP BY s.provincia, s.unitamisura;
+            """
+            df = pd.read_sql_query(query, conn, params=(pollutant, start_date, end_date))
+        conn.close()
+        return jsonify(df.to_dict(orient='records'))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+# Server run
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
