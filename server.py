04fd1c3cd7a979ef4f369e86d529da8e55b33812
alessia-ippolito <alessia.ippo01@gmail.com>
2025-06-30 16:07:57 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -22,6 +22,80 @@ def db_connection():
     return conn
 
 
+def check_user_exists(username):
+    conn = db_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+    cursor.close()
+    return 1 if result else 0
+
+# Login
+@app.route('/api/login', methods=['POST'])
+def login():
+    conn = db_connection()
+    # query to retrieve user data
+    sql_query = "SELECT * FROM users;"
+    df_user = pd.read_sql_query(sql_query, conn)
+    print('Dataframe:')
+    print(df_user.head(), '\n') 
+
+    #global df_user
+    global logged_in
+    logged_in = False
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    # if the username or password is empty
+    if not username or not password:
+        return jsonify({"message": "Username and password are required"}), 400
+    # if the username or email is incorrect
+    elif username not in df_user['username'].values and username not in df_user['email'].values:
+            return jsonify({"message": "Invalid username"}), 401
+    # if the username is correct
+    elif username in df_user['username'].values or username in df_user['email'].values:
+        # check if the password is correct
+        match = df_user[df_user['username'] == username]
+        pw = match['password'].values[0]
+        # if the password is incorrect
+        if  pw != password:
+            return jsonify({"message": "Invalid password"}), 403
+        # if the password is correct
+        else: 
+            logged_in = True
+            return jsonify({"message": "Login successful"}), 200
+    else: 
+        return jsonify({"message": "ERROR"}),500
+
+# Signin
+@app.route('/api/signin', methods=['POST'])
+def signin():
+    global logged_in
+    conn = db_connection()
+    new_data = request.get_json()
+    username = new_data.get('username')
+    email = new_data.get('email')
+    password = new_data.get('password')
+    # if the username or password or email is empty
+    if not username or not password or not email:
+        return jsonify({"message": "All fields are necessary to be registered"}), 400
+    # if the username or email is already taken
+    elif check_user_exists(username) == 1:
+        return jsonify({"message": "Username already exists"}), 409
+    else: 
+        try:  
+            cursor = conn.cursor()
+            query = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
+            cursor.execute(query, (username, email, password))
+            conn.commit()
+            logged_in = True
+            return jsonify({"message": "Signin successful"}), 200
+        except Exception as e:
+            return jsonify({"message": f"Internal server error: {str(e)}"}), 500
+        
+    
 @app.route('/api/provinces', methods=['GET'])
 def get_provinces():
     """
@@ -159,8 +233,6 @@ def api_get_measurements():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
-
-
 
 @app.route('/api/measurements_by_province', methods=['GET'])
 def get_measurements_by_province():
